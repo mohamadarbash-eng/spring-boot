5e7d27cd0a14ffc205b899ea15ad19463576e04b
Arbash Mohamad <eng@mohamadarbash.com>
2020-04-11 11:59:08 +0200

    Config Java Beans with code, no xml

---
+++ spring-anotation/src/com/anotation/mo/JavaConfig.java
@@ -0,0 +1,20 @@
+package com.anotation.mo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfig {
+
+	public static void main(String[] args) {
+		
+	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+	
+	Coach theCoach = context.getBean("tennisCoach", Coach.class);
+	
+	System.out.println(theCoach.getDailyWorkout());
+	System.out.println(theCoach.getDailyFortune());
+	
+	context.close();
+	
+	}
+
+}
---
+++ spring-anotation/src/com/anotation/mo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.anotation.mo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration()
+@ComponentScan("com.anotation.mo")
+public class SportConfig {
+
+}
