0443148d03b411b24cd3cd5492c5c87dd8c34479
Arbash Mohamad <eng@mohamadarbash.com>
2020-04-11 11:30:23 +0200

    Annotation Scope

---
+++ spring-anotation/src/com/anotation/mo/AnnotationBeanScope.java
@@ -0,0 +1,21 @@
+package com.anotation.mo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScope {
+
+	public static void main(String[] args) {
+
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+		
+		System.out.println(theCoach == alphaCoach);
+		context.close();
+		
+	}
+
+}
