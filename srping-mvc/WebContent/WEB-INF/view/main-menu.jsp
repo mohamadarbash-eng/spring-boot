f16e2d895c0f3b434cd13373d0c1248164d28277
Arbash Mohamad <eng@mohamadarbash.com>
2020-04-12 20:39:02 +0200

    using model Attribute / form tags / data Binding

--- srping-mvc/WebContent/WEB-INF/view/main-menu.jsp
+++ srping-mvc/WebContent/WEB-INF/view/main-menu.jsp
@@ -6,5 +6,6 @@ HI I am Main Menu
 </h2>
 <a href="hello/showForm">go to show Form</a>
 <a href="bye/showForm"> bye world</a>
+<a href="student/showForm">Student</a>
 </body>
 </html>
---
+++ srping-mvc/src/com/springmvc/mo/StudentController.java
@@ -0,0 +1,27 @@
+package com.springmvc.mo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	
+	@RequestMapping("/showForm")
+	public String ShowForm(Model theModel) {
+		
+		Student theStudent = new Student();
+		
+		theModel.addAttribute("student", theStudent);
+		
+	return "student-form";	
+	}
+	
+	
+	@RequestMapping("/processForm") 
+	public String processForm(@ModelAttribute("student") Student theStudent){
+		return "student-confirmation";
+	}
+}
